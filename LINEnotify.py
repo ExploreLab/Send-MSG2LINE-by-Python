de8b6d2ab2d889c0b8169005c78eb12d3f97db89
Chief Engineer <jukrapun.ch@gmail.com>
2017-12-21 15:38:07 +0700

    Send message to LINE Application using Python Up to date Send msg/Sticker/Picture

--- LINEnotify.py
+++ LINEnotify.py
@@ -6,12 +6,12 @@ def LINEnotify(msg):
     ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
     # msg = 'ETIC LAB - PClab'
 
-    # payload = {'message': msg, 'stickerPackageId': 4, 'stickerId': 629} #2/39 3/198
-    payload = {'message': msg}
+    # payload = {'message': msg}
+    payload = {'message': msg, 'stickerPackageId': 4, 'stickerId': 629} #2/39 3/198
     headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
-    # files = {"imageFile": open("HOME.jpg", "rb")}
-    r = requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers)
-    # r = requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers, files=files)
+    files = {"imageFile": open("HC-12.jpg", "rb")}
+    # r = requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers)
+    r = requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers, files=files)
     print(r)
     return r
 
